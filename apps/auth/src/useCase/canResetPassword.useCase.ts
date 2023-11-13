891e33a8edc4d10a8a04dca5752befe728724ea7
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2023-11-13 08:47:40 -0300

    ✨feature: Adding rules on reset password with code

--- apps/auth/src/useCase/canResetPassword.useCase.ts
+++ apps/auth/src/useCase/canResetPassword.useCase.ts
@@ -1,7 +1,13 @@
 import { PasswordResetToken } from '@app/common/database';
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as moment from 'moment';
 import { Repository } from 'typeorm';
+
+export interface CanResetPasswordResponse {
+  success: boolean;
+  message: string;
+}
 
 export class CanResetPasswordUseCase {
   constructor(
@@ -9,17 +15,36 @@ export class CanResetPasswordUseCase {
     private passwordResetRepository: Repository<PasswordResetToken>,
   ) {}
 
-  async execute(token: string, code: string): Promise<boolean> {
-    const resetToken: PasswordResetToken =
-      await this.passwordResetRepository.findOneBy({ token });
+  async execute(
+    token: string,
+    code: string,
+  ): Promise<CanResetPasswordResponse> {
+    try {
+      const resetToken: PasswordResetToken =
+        await this.passwordResetRepository.findOneBy({ token });
 
-    if (resetToken) {
-      return (
+      if (
+        resetToken &&
         moment(resetToken.expiryDate).isAfter(moment()) &&
         resetToken.code === code
+      ) {
+        return {
+          success: true,
+          message: 'Código válidado com sucesso',
+        };
+      }
+
+      throw new HttpException('', HttpStatus.NOT_FOUND);
+    } catch (error) {
+      console.log(`error for resetting password: ${error}`);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
-
-    return false;
   }
 }
