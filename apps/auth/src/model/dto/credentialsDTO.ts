a9f7b143976825514bf24d6994db6a66ab5ede66
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2024-02-22 16:04:10 -0300

    fix: remove swagger on creadentialsDTO

--- apps/auth/src/model/dto/credentialsDTO.ts
+++ apps/auth/src/model/dto/credentialsDTO.ts
@@ -1,12 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CredentialsDTO {
-  @ApiProperty()
   @IsEmail()
   email: string;
 
-  @ApiProperty()
   @IsNotEmpty()
   password: string;
 }
